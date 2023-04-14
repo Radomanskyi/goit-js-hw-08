baf5a5a3fcd30475c78ddc78d51fb9eedf02ff51
Radomanskyi <114738589+Radomanskyi@users.noreply.github.com>
2023-04-14 23:51:08 +0300

    DZ_8_3/3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,15 @@
 import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
 const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }, 1000)
+);
+
+const timeFromStorage = localStorage.getItem(`videoplayer-current-time`) || 0;
+player.setCurrentTime(JSON.parse(timeFromStorage));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import { throttle } from 'lodash';
 
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const button = document.querySelector('button');
+
+email.addEventListener('input', throttle(onInputEmail, 500));
+message.addEventListener('input', throttle(onInputMessage, 500));
+button.addEventListener('click', onSub);
+
+const currentObj =
+  JSON.parse(localStorage.getItem('feedback-form-state')) ?? '';
+email.value = currentObj.email || '';
+message.value = currentObj.message || '';
+
+let resultObj = {
+  email: currentObj.email || '',
+  message: currentObj.message || '',
+};
+
+function onInputEmail() {
+  resultObj.email = email.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(resultObj));
+}
+
+function onInputMessage() {
+  resultObj.message = message.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(resultObj));
+}
+
+function onSub(event) {
+  if (email.value === '' || message.value === '') {
+    alert('Заполните все поля');
+  } else {
+    event.preventDefault();
+    const localData = localStorage.getItem('feedback-form-state');
+    console.log(JSON.parse(localData));
+    email.value = '';
+    message.value = '';
+    localStorage.removeItem('feedback-form-state');
+    resultObj = {};
+  }
+}
