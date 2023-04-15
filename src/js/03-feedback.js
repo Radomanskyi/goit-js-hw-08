ac51d9506292078c07a9ce0b9ee1128cf41a5947
Radomanskyi <114738589+Radomanskyi@users.noreply.github.com>
2023-04-15 15:32:08 +0300

    eror 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,6 +3,7 @@ import { throttle } from 'lodash';
 const email = document.querySelector('input');
 const message = document.querySelector('textarea');
 const button = document.querySelector('button');
+const form = document.querySelector("form");
 
 email.addEventListener('input', throttle(onInputEmail, 500));
 message.addEventListener('input', throttle(onInputMessage, 500));
@@ -29,10 +30,10 @@ function onInputMessage() {
 }
 
 function onSub(event) {
+  event.preventDefault();
   if (email.value === '' || message.value === '') {
     alert('Заполните все поля');
   } else {
-    event.preventDefault();
     const localData = localStorage.getItem('feedback-form-state');
     console.log(JSON.parse(localData));
     email.value = '';
